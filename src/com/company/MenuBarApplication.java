c146a3d61e63ae9c77879669462e972a074cb334
Dove <bocz1998@gmail.com>
2022-12-06 22:35:11 +0100

    Beginning

--- src/com/company/MenuBarApplication.java
+++ src/com/company/MenuBarApplication.java
@@ -10,44 +10,29 @@ public class MenuBarApplication extends JMenuBar {
     private JCheckBoxMenuItem onlyForRead = new JCheckBoxMenuItem("only for read");
     private JRadioButtonMenuItem radio;
 
-    MenuBarApplication(){
+    MenuBarApplication() {
         initComponents();
     }
 
-    public void initComponents()
-    {
+    public void initComponents() {
         JMenu menuFile = this.add(new JMenu("File"));
         JMenuItem subMenu = menuFile.add(new JMenuItem("New"));
 
-        subMenu.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Tu jest kod, który tworzy nowy plik");
-            }
-        });
+        subMenu.addActionListener(e -> System.out.println("Tu jest kod, który tworzy nowy plik"));
 
-        menuFile.addSeparator();
-        final JMenuItem podMenuZapisz = menuFile.add(new JMenuItem("Save"));
-        menuFile.add(new JMenuItem("Load"));
         menuFile.addSeparator();
         JMenu menuOpcje = new JMenu("Options");
         menuFile.add(menuOpcje);
 
-        menuOpcje.add(new JMenuItem("Option 1"));
-        menuOpcje.add(new JMenuItem("Option 2"));
+        menuOpcje.add(new JMenuItem("Version 1"));
+        menuOpcje.add(new JMenuItem("Version 2"));
         menuOpcje.add(onlyForRead);
 
-        onlyForRead.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e)
-            {
-                if (onlyForRead.isSelected())
-                    onlyForRead.setEnabled(false);
-                else
-                    onlyForRead.setEnabled(true);
-            }
+        onlyForRead.addActionListener(e -> {
+            onlyForRead.setEnabled(!onlyForRead.isSelected()); // sprawdzić czy działa
         });
 
-        JMenu menuPomoc = this.add(new JMenu("Help"));
+        JMenu menuPomoc = this.add(new JMenu("Test"));
 
         menuPomoc.add(new JMenuItem("FAQ"));
 
