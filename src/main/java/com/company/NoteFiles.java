f6f112114d3df86b9ea46cd56570a15761eab69e
Dove <bocz1998@gmail.com>
2023-03-02 04:38:38 +0100

    Beginning

--- src/main/java/com/company/NoteFiles.java
+++ src/main/java/com/company/NoteFiles.java
@@ -2,28 +2,47 @@ package com.company;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
 
 public class NoteFiles {
 
-    public void noteFilesExecute() {
+//    public void noteFilesExecute() {
+//
+//        try {
+//
+//            RandomAccessFile RAF = new RandomAccessFile("nowy.txt", "rw");
+//            StringBuffer buffer = new StringBuffer();
+//
+//
+//            while(RAF.getFilePointer() < RAF.length()) {
+//                buffer.append(RAF.readLine()+System.lineSeparator());
+//            }
+//            String contents = buffer.toString();
+//            System.out.println("Contents of the file : "+contents);
+//
+//
+//            RAF.close();
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
 
-        try {
+    public void noteFilesExecute2(){
+            // Dane do zapisania
+            String data = "Hello, World!";
 
-            RandomAccessFile RAF = new RandomAccessFile("nowy.txt", "rw");
-            StringBuffer buffer = new StringBuffer();
+            // Utwórz obiekt File dla pliku, do którego chcesz zapisać dane
+            File file = new File("example.txt");
 
-
-            while(RAF.getFilePointer() < RAF.length()) {
-                buffer.append(RAF.readLine()+System.lineSeparator());
+            try {
+                // Użyj funkcji FileUtils.write() do zapisu danych do pliku
+                FileUtils.write(file, data, "UTF-8");
+                System.out.println("Dane zostały zapisane do pliku.");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            String contents = buffer.toString();
-            System.out.println("Contents of the file : "+contents);
-
-
-            RAF.close();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
     }
 
 }
---
+++ src/main/java/com/company/PanelCenterV2.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelCenterV2 extends JPanel {
+
+    protected JTextField textCenter1 = new JTextField("Title");
+    private JTextField textCenter2 = new JTextField("Contents");
+
+
+    PanelCenterV2(){
+        initComponentsPanel();
+    }
+
+
+    public void initComponentsPanel(){
+        this.setPreferredSize(new Dimension(280, 250));
+        textCenter1.setPreferredSize(new Dimension(280, 50));
+        textCenter2.setPreferredSize(new Dimension(280, 200));
+
+        this.setLayout(new BorderLayout());
+        this.add(textCenter1, BorderLayout.PAGE_START);
+        this.add(textCenter2, BorderLayout.PAGE_END);
+    }
+}
